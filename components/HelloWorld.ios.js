9f24f80d9fd97339bdf7d7e714a8b5a1dcfe6563
prandriasony <fenitra.pacome@gmail.com>
2019-07-06 13:13:51 +0300

    test de commit

---
+++ components/HelloWorld.ios.js
@@ -0,0 +1,15 @@
+// Components/HelloWorld.android.js
+
+import React from 'react'
+import { Text } from 'react-native'
+
+class HelloWorld extends React.Component {
+
+  render() {
+    return (
+      <Text>Hello ios</Text>
+    )
+  }
+}
+
+export default HelloWorld
